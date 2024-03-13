aac033ca150628a73e907020e8434caf69f1bbfe
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-03-13 11:46:13 -0400

    size class constructors

---
+++ inc/utils/size.h
@@ -0,0 +1,39 @@
+#ifndef S2_SIZE_H
+#define S2_SIZE_H
+
+
+#include <type_traits>
+
+
+namespace s2 {
+    template <
+        typename T,
+        typename = typename std::enable_if_t<std::is_arithmetic_v<T>>
+    >
+    class size;
+
+
+    template <typename T>
+    class size<T> {
+    public:
+        using value_type = T;
+    public:
+        constexpr size(const value_type _w, const value_type _h) noexcept:
+            _w(_w), _h(_h) {}
+
+        constexpr size(const value_type _n) noexcept:
+            _w(_n), _h(_n) {}
+
+        constexpr size(const size& _size) noexcept:
+            _w(_size._w), _h(_size._h) {}
+
+        constexpr size() noexcept {}
+
+        ~size() noexcept {}
+    private:
+        T _w;
+        T _h;
+    };
+}
+
+#endif // S2_SIZE_H
