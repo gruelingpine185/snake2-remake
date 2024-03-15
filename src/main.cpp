2aa475ed65e91053cbd211c588828fe15bf26096
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-03-15 12:11:20 -0400

    finally... an empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+#include <SDL3/SDL.h>
+
+#include "window.h"
+
+
 int main() {
+    s2::window window;
+    if(!window.create("Snake 2", {800, 600})) return 1;
+    if(!window.show()) return 1;
+
+    while(!window.should_close()) {
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+            window.handle_events(e);
+        }
+
+        if(!window.clear_screen()) return 1;
+        if(!window.render()) return 1;
+    }
+
     return 0;
 }
