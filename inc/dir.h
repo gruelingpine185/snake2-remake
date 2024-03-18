13e90164dea0504ed42998d6c421f767971999f6
Adrian <66073333+gruelingpine185@users.noreply.github.com>
2024-03-18 00:04:44 -0400

    added direction and axis enums

---
+++ inc/dir.h
@@ -0,0 +1,23 @@
+#ifndef S2_DIR_H
+#define S2_DIR_H
+
+
+namespace s2 {
+    enum class dir;
+    enum class axis;
+
+
+    enum class dir {
+        up,
+        down,
+        left,
+        right
+    };
+
+    enum class axis {
+        x,
+        y
+    };
+}
+
+#endif // S2_DIR_H
